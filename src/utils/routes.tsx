5f05ba8bc4100cf8127137bb0ac4a90a897f0d5d
isabeladuarte <isabeladuartebg@gmail.com>
2022-12-06 19:32:04 -0300

    fix: fix redirect in edit aupair route

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -3,7 +3,22 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedRoute: React.FC = () => {
   const hasUser = sessionStorage.getItem('accessToken')
-  return hasUser ? <Outlet /> : <Navigate to="/login" />
+  const role = sessionStorage.getItem('role')
+  const notFirstLoginStorage = localStorage.getItem('notFirstLogin')
+  const notFirstLogin = notFirstLoginStorage && JSON.parse(notFirstLoginStorage)
+  const path = location.pathname
+
+  if (hasUser) {
+    if (role === 'ROLE_AUPAIR') {
+      if (path !== '/edit_aupair') {
+        if (!notFirstLogin) return <Navigate to="/edit_aupair" />
+        else return <Outlet />
+      } else if (notFirstLogin) return <Navigate to="/jobs" />
+    }
+    return <Outlet />
+  }
+
+  return <Navigate to="/login" />
 }
 
 const UnProtectedRoute: React.FC = () => {
